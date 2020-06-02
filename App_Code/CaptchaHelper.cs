a5ea1e47f048d56be9713a1d86e5cfde25931cdd
Hoàng Huy Nguyễn <nhhuy1804.dev@gmail.com>
2020-06-02 22:32:35 +0700

    first commit

---
+++ App_Code/CaptchaHelper.cs
@@ -0,0 +1,53 @@
+﻿using BotDetect.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DPDSP.App_Code
+{
+    public class CaptchaHelper
+    {
+        public static MvcCaptcha GetRegistrationCaptcha()
+        {
+            // create the control instance
+            MvcCaptcha registrationCaptcha = new MvcCaptcha("RegistrationCaptcha");
+
+            // set up client-side processing of the Captcha code input textbox
+            registrationCaptcha.UserInputID = "CaptchaCode";
+
+            // Captcha settings
+            registrationCaptcha.ImageSize = new System.Drawing.Size(255, 50);
+
+            return registrationCaptcha;
+        }
+
+        public static MvcCaptcha GetLoginCaptcha()
+        {
+            // create the control instance
+            MvcCaptcha registrationCaptcha = new MvcCaptcha("LoginCaptcha");
+
+            // set up client-side processing of the Captcha code input textbox
+            registrationCaptcha.UserInputID = "CaptchaCode";
+
+            // Captcha settings
+            registrationCaptcha.ImageSize = new System.Drawing.Size(255, 50);
+
+            return registrationCaptcha;
+        }
+
+        public static MvcCaptcha GetChangePasswordCaptcha()
+        {
+            // create the control instance
+            MvcCaptcha registrationCaptcha = new MvcCaptcha("ChangePasswordCaptcha");
+
+            // set up client-side processing of the Captcha code input textbox
+            registrationCaptcha.UserInputID = "CaptchaCode";
+
+            // Captcha settings
+            registrationCaptcha.ImageSize = new System.Drawing.Size(255, 50);
+
+            return registrationCaptcha;
+        }
+    }
+}
---
+++ Scripts/BEN.js
@@ -0,0 +1,28 @@
+﻿/*global window */
+
+(function (TX, $) {
+    var url = "";
+    TX.showDialog = function (sender) {
+        url = sender.src;
+        var dialog = $("#dialog").data("kendoDialog");
+        var dataSource = $(dialog.element.context.firstChild).data("kendoListView").dataSource;
+        dataSource.fetch();
+        dialog.open();
+    }
+
+    TX.listViewDataBound = function () {
+        var dialog = $("#dialog").data("kendoDialog");
+        if (this.dataSource.data().length != 0) {
+            dialog.center();
+            dialog.title(this.dataSource.data()[0].AttractionTitle);
+        }
+    }
+
+    TX.imageInfo = function () {
+        return { imageURL: url };
+    }
+
+    TX.contact_showDialog = function () {
+        $("#dialog").data("kendoDialog").open();
+    }
+})(window.TX = window.TX || {}, $);
